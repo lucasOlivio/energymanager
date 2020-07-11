cdbe4b0426195fffa32adb99d3df6e6b6958222d
Lucas Olivio <lucas27_olivio@hotmail.com>
2020-07-11 13:44:23 -0300

    readme and comments

---
+++ README.md
@@ -0,0 +1,3 @@
+## Energy manager
+
+Endpoint and website for receiving and showing arduino data on current. An energy manager for you to control all that is happening in any room of your business or home.
--- raspberry/com_arduino.py
+++ raspberry/com_arduino.py
@@ -5,10 +5,10 @@ import select
 import traceback
 from conectarBD import Conect
 
+# Estabilishes connection with DB and define Arduino's connection data
 conection = Conect()
 
 UDP_IP = "192.168.15.20"
-UDP_ARDUINO = "192.168.15.21"
 UDP_PORT = 5005
 MESSAGE = "OK"
 GC = "1"
@@ -17,6 +17,7 @@ print ("UDP target IP:", UDP_IP)
 print ("UDP target port:", UDP_PORT)
 print ("message:", MESSAGE)
 
+# Starts listening socket
 sock = socket.socket(socket.AF_INET, # Internet
 					 socket.SOCK_DGRAM) # UDP
 
@@ -26,7 +27,7 @@ sock.bind((UDP_IP, UDP_PORT))
 
 try:
 	while True:
-
+		# Receives data from arduino and insert on DB
 		ready = select.select([sock], [], [], 0)
 		if ready[0]:
 			data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
@@ -57,6 +58,7 @@ try:
 			sock.sendto(GC.encode(), (arduino[0], UDP_PORT))
 					
 except Exception as e:
+	# If some problem occurs closes socket and db connection
 	Te = sys.exc_info()[0]
 	print ("{0}".format(e))
 	print (Te)
